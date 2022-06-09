646b92e23efce35212cf57d4a8a9b567003e9836
salgu1998 <salgu1998@gmail.com>
2022-06-09 23:02:23 +0900

    first commit

---
+++ src/test/java/com/ecommerceconfig/EcommerceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
